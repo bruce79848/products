164ce74efd79f1304087702032dc64a10679c90a
Bruce <bruce79848@gmail.com>
2020-03-21 15:38:35 +0800

    Add a function of automatically generating a file

--- product.py
+++ product.py
@@ -4,8 +4,14 @@ while True:
 	if name == 'q':
 		break
 	price = input('Please input the product price: ')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
 for p in products:
 	print(p[0], ',the price is', p[1])
+
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
